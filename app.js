b22e9789b882f26eb9ea42976d2c4e3ab44f478d
Mantelis01 <mantas.zukovskis@gmail.com>
2023-02-20 21:25:21 +0200

    Finished v1 of the app. Will add some styling later. v2 expected to be more difficult AI.

--- app.js
+++ app.js
@@ -16,19 +16,52 @@ function Game() {
     }
 
     function takeTurn() {
-
+        if (board.checkForWinner()) {
+            return;
+        }
         if (turn % 2 === 0) {
             humanPlayer.takeTurn();
         } else {
             computerPlayer.takeTurn();
         }
-
         turn++;
     }
 }
 
 function Board() {
     this.position = Array.from(document.querySelectorAll('.square'))
+    this.checkForWinner = function () {
+        let winner = false
+        const winningCombinations = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8]
+        ];
+        const positions = this.position;
+
+        winningCombinations.forEach((winningCombo) => {
+            const pos0InnerText = positions[winningCombo[0]].innerText;
+            const pos1InnerText = positions[winningCombo[1]].innerText;
+            const pos2InnerText = positions[winningCombo[2]].innerText;
+            const isWinningCombo = pos0InnerText !== '' &&
+                pos0InnerText === pos1InnerText &&
+                pos1InnerText === pos2InnerText;
+
+
+            if (isWinningCombo) {
+                winner = true;
+                winningCombo.forEach((index) => {
+                    positions[index].className += ' Winner';
+                });
+            }
+        })
+        return winner;
+    }
 }
 
 function HumanPlayer(board) {
@@ -46,7 +79,8 @@ function ComputerPlayer(board) {
 
     this.takeTurn = function () {
         const availablePositions = board.position.filter((p) => p.innerText === '')
-        console.log(availablePositions)
+        const move = Math.floor(Math.random() * availablePositions.length)
+        availablePositions[move].innerText = 'O';
     }
 
 }
